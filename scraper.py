6623511cd1ff1d3ad96d04ce0f5325e488aff628
Trimpta <68428753+trimpta@users.noreply.github.com>
2024-01-21 14:24:52 +0530

    now accepts json object instead of file obj

--- scraper.py
+++ scraper.py
@@ -3,47 +3,49 @@ import requests
 import csv
 
 
-def game(val,writer_obj,text = False):
-    leader_board = json.load(val)
-    num = 1
+def game(leader_board,writer_obj,text = False):
 
-    for player in leader_board[0]["result"]["data"]["rows"]:
+    try:
+        for player in leader_board[0]["result"]["data"]["rows"]:
 
-        uiid = player['user_id']
-        url = f"https://social-api.drift.io/trpc/game.getStats?batch=1&input=%7B%220%22%3A%7B%22id%22%3A%22{uiid}%22%7D%7D"
-        respons = requests.get(url)
-        
-        data = respons.json()
-        userdata = data[0]["result"]["data"]["profile"]
-        name = userdata["username"]
-        xp = userdata["experience"]
-        playtime = userdata["playtime"]
-        drift = userdata["distance_drifted"]
-        driven = userdata["distance_driven"]
-        money = userdata["coins"]
-        skill = userdata["skill_rating"]
-        level = data[0]["result"]["data"]["level"]["level"]
-        created_at = userdata["created_at"]
-
-        writer_obj.writerow([name,uiid,playtime,xp,level,money,driven,drift,skill,created_at])      
-
-        if userdata["admin"]:
-            print(f"Admin spotted lol {uiid}:{name}")
-            if text:
-                text.write(f"Admin spotted lol {uiid}:{name}")
+            uiid = player['user_id']
+            url = f"https://social-api.drift.io/trpc/game.getStats?batch=1&input=%7B%220%22%3A%7B%22id%22%3A%22{uiid}%22%7D%7D"
+            respons = requests.get(url)
             
+            data = respons.json()
+            userdata = data[0]["result"]["data"]["profile"]
+            name = userdata["username"]
+            xp = userdata["experience"]
+            playtime = userdata["playtime"]
+            drift = userdata["distance_drifted"]
+            driven = userdata["distance_driven"]
+            money = userdata["coins"]
+            skill = userdata["skill_rating"]
+            level = data[0]["result"]["data"]["level"]["level"]
+            created_at = userdata["created_at"]
 
-        
-        print(num)
-        num+=1
+            writer_obj.writerow([name,uiid,playtime,xp,level,money,driven,drift,skill,created_at])      
 
+            if userdata["admin"]:
+                print(f"Admin spotted lol {uiid}:{name}")
+                if text:
+                    text.write(f"Admin spotted lol {uiid}:{name}")
+                
+
+            
+            print(num)
+            num+=1
+
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
     with open("data.json","r") as f, open("data.csv","w", encoding="utf-8",newline="") as csvfile, open("levellist.txt","w", encoding="utf-8") as levels:
-        
+        lb = json.load(f)
         wrter = csv.writer(csvfile)
         wrter.writerow(["name","uiid","playtime","xp","level","money","driven","drift","skill","created_at"])
-        game(f,wrter,levels)
+        
+        game(lb,wrter,levels)
 
     print("program executed")
